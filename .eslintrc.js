34703536ecaafe81e403338cacd50254edad1b91
mikehuebner <mikehuebner@me.com>
2023-09-24 20:46:54 -0700

    chore: initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:eslint-plugin/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'unused-imports', 'import'],
+  env: {
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', ['parent', 'sibling'], 'index', 'unknown'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
+      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
+    ],
+  },
+  overrides: [
+    {
+      files: ['tests/**/*.js'],
+      env: { mocha: true },
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,48 @@
+# eslint-plugin-tamagui
+
+An organizer for props and styled components
+
+## Installation
+
+You'll first need to install [ESLint](https://eslint.org/):
+
+```sh
+npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-tamagui`:
+
+```sh
+npm install eslint-plugin-tamagui --save-dev
+```
+
+## Usage
+
+Add `tamagui` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "tamagui"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "tamagui/rule-name": 2
+    }
+}
+```
+
+## Rules
+
+<!-- begin auto-generated rules list -->
+TODO: Run eslint-doc-generator to generate the rules list.
+<!-- end auto-generated rules list -->
+
+
